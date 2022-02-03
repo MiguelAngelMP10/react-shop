f580ffa18fc707266f15a5bdc4aa4b0658478054
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-02 21:51:01 -0600

    Estilos en los componentes de React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import '../styles/global.scss';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 const App = () => {
 	return (
-		<h1>Hola Mundo con cambio!</h1>
+		<Layout>
+			<Login />
+		</Layout>
 	);
 }
 
