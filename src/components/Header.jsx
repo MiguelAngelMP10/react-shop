247b957a62e9c5ac00b38c60dd6d293df84d34cd
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-11 09:25:04 -0600

    Completando el carrito de compras

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/Menu'
 
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
+import AppContext from '../context/AppContext';
+
 
 const Header = () => {
 
 	const [toggle, setToggle] = useState(false);
+	const { state } = useContext(AppContext);
+
 
 	const handleToggle = () => {
 		setToggle(!toggle)
@@ -47,7 +51,7 @@ const Header = () => {
 					</li>
 					<li className="navbar-shopping-cart">
 						<img src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
+						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
