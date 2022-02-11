c8ca0ca1c508b6dc7fd532e603bb37b7baac0827
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-11 10:10:29 -0600

    Orden de compra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
-import Menu from '@components/Menu'
+import Menu from '@components/Menu';
+import MyOrder from '../containers/MyOrder';
 
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
@@ -11,6 +12,7 @@ import AppContext from '../context/AppContext';
 const Header = () => {
 
 	const [toggle, setToggle] = useState(false);
+	const [toggleOrders, setToggleOrders] = useState(false);
 	const { state } = useContext(AppContext);
 
 
@@ -49,13 +51,14 @@ const Header = () => {
 					<li className="navbar-email" onClick={handleToggle}>
 						platzi@example.com
 					</li>
-					<li className="navbar-shopping-cart">
+					<li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
 						<img src={shoppingCart} alt="shopping cart" />
 						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
 			{toggle && <Menu />}
+			{toggleOrders && <MyOrder />}
 		</nav>
 	);
 }
